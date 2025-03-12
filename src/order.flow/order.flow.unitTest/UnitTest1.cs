1915d795a1fa6423f3459a348895482a855d52f3
Galdino <emersongaldino@hotmail.com>
2025-03-12 10:53:30 -0300

    feat: add solution unitTest

---
+++ src/order.flow/order.flow.unitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace order.flow.unitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
