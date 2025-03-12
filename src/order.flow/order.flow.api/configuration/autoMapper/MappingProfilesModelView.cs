aeb3989c1434c904dca4d8454600d543056c3cce
Galdino <emersongaldino@hotmail.com>
2025-03-12 11:21:24 -0300

    feat: add profile settings api

---
+++ src/order.flow/order.flow.api/configuration/autoMapper/MappingProfilesModelView.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace order.flow.api.configuration.autoMapper;
+
+public class MappingProfilesModelView : Profile
+{
+    public MappingProfilesModelView()
+    {
+        // CreateMap<UserModel, UserModelView>().ReverseMap();
+        // CreateMap<RouteEntity, RouteModelView>().ReverseMap();
+        // CreateMap<RouteEntity, RouteViewModel>().ReverseMap();
+    } 
+}
