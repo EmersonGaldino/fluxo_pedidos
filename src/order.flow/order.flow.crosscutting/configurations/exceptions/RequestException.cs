a1f30bf282f6169273d84158d307cb5d47d09409
Galdino <emersongaldino@hotmail.com>
2025-03-12 11:28:41 -0300

    feat: add requestException

---
+++ src/order.flow/order.flow.crosscutting/configurations/exceptions/RequestException.cs
@@ -0,0 +1,12 @@
+namespace order.flow.crosscutting.configurations.exceptions;
+
+public class RequestException : Exception
+{
+    public string ErrorMessage { get; set; }
+    public int StatusCode { get;  }
+    public RequestException(int statussCode, string message)
+    {
+        StatusCode = statussCode;
+        ErrorMessage = message;
+    }
+}
